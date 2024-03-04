7f00c95bd274c193f7e6245c7d372c0970d7a70f
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-03-04 22:05:46 +0300

    right sidebar

--- src/layouts/main/rightbar/topics/index.jsx
+++ src/layouts/main/rightbar/topics/index.jsx
@@ -1,20 +1,15 @@
 import { Link } from "react-router-dom";
 import { topics } from "../../../../utils/consts";
 import Topic from "./topic";
+import SidebarSection from "../../../../components/sidebar-section";
 
 export default function Topics() {
     return (
-        <section className="bg-[#16181c] overflow-hidden mb-4 rounded-2xl border border-[#16181c]">
-            <h5 className="py-3 px-4 text-xl font-extrabold leading-6 flex items-center text-[#e7e9ea]">Trends for you</h5>
-            <div className="grid">
-                {topics.map((topic, index) => <Topic item={topic} key={index} />)}
-                <Link 
-                    to="/trends"
-                    className="h-[52px] flex items-center px-4 text-[15px] text-[#1d9bf0] transition-colors hover:bg-white/[0.03] "
-                >
-                    Show more
-                </Link>
-            </div>
-        </section>
+        <SidebarSection 
+            title="Trends for you"
+            more="/trends"
+        >
+            {topics.map((topic, index) => <Topic item={topic} key={index} />)}
+        </SidebarSection>
     )
 }
---
+++ src/mock/index.js
@@ -0,0 +1,25 @@
+export const whoFollowUsers = [
+  {
+    id: 3,
+    username: "elonmusk",
+    fullName: "Elon Musk",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_x96.jpg",
+    verified: true,
+  },
+  {
+    id: 4,
+    username: "BilisimVadisiTR",
+    fullName: "Bilişim Vadisi",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1734118032820084736/JCviFAf8_x96.jpg",
+    verified: true,
+  },
+  {
+    id: 5,
+    username: "isbilir_es",
+    fullName: "Esma İşbilir",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1761707710544855040/W46IU3-o_400x400.jpg",
+  },
+];
